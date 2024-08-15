ba043dccc5b73bc42b95be0e00cc3d88ca612dfb
Clemens Holleis <clemens.holleis@gmail.com>
2024-08-15 12:41:30 +0200

    fixed index creation

--- build_index.py
+++ build_index.py
@@ -3,11 +3,10 @@ from pathlib import Path
 import urllib.parse
 from textwrap import dedent, indent
 
-
+INDENT = lambda x=1 : " "*(4*x)
 class IndexBuilder:
     def __init__(self) -> None:
         self.root = Path(__file__).parent
-        self.template = (self.root / 'index.twig').read_text()
         self.index = (self.root / 'index.html')
 
     @property
@@ -21,21 +20,30 @@ class IndexBuilder:
     @property
     def link_names_urls(self) -> list[tuple[str, str]]:
         return [(file.rsplit('/',maxsplit=1)[0].replace('/', ' > '), urllib.parse.quote(file)) for file in self.rel_index_files_str]
+    
+    @property
+    def template(self) -> str:
+        txt = self.index.read_text()
+        a,b,_ = txt.partition('<ul>')
+        _,c,d = txt.rpartition('</ul>')
+
+        return f'{a}{b}%%list%%{INDENT(1)}{c}{d}'
+
 
     @property
     def html_links(self) -> str:
         raw = [f"""
                 <li>
-                    <a href="{url}">{name}</ul>
+                    <a href="{url}">{name}</a>
                 </li>
                 """
                 for name, url in self.link_names_urls
             ]
-        return indent(dedent("".join(raw)), "       ").strip()
+        return indent(dedent("".join(raw)), INDENT(2))
     
     def build(self):
         self.index.write_text(
-            self.template.replace('{{list}}', self.html_links)
+            self.template.replace('%%list%%', self.html_links)
         )
 
 if __name__ == "__main__":
